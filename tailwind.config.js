3bf789d410e67da128491c642f68248785ea161d
Ramvishvas Kumar <ramvishvas.kumar@primathon.in>
2024-04-09 10:30:02 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                poppins: ['Poppins', 'sans'],
+                roboto: ['Roboto', 'sans-serif'],
+                serif: ['DM Serif Display', 'serif']
+            },
+            screens: {
+                '3xl': '1920px'
+            }
+        }
+    },
+    plugins: []
+};
