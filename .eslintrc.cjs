ce0219f4e86027c6fe0db90f7830e67af0cce697
Mukul Deval <118657142+Mukul-Deval1999@users.noreply.github.com>
2024-04-11 20:24:53 +0530

    [Feature/GetTouch]:Created mobile responsive GetTouch section

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
     plugins: ['react-refresh'],
     rules: {
         'react/jsx-no-target-blank': 'off',
-        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        'prettier/prettier': ['error', {endOfLine: 'auto'}]
     }
 };
