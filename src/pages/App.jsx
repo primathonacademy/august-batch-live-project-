e9ba0d16d99e5663fdc6f340cd50d7b0215570dd
unknown <imtriloksingh2000@gmail.com>
2024-04-15 00:17:00 +0530

    [Feature]: Add cofounder component

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,5 +1,7 @@
+import CoFounder from '../components/CoFounder';
+
 const App = () => {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    return <CoFounder></CoFounder>;
 };
 
 export default App;
