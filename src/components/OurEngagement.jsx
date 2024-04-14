b8cf241bc9c203238c0161de795c1912a38d224c
sakshimandloi <sakshimandloi4722@gmail.com>
2024-04-14 17:42:13 +0530

    [Feature]: Update tailwind classes in ourengagement model component

--- src/components/OurEngagement.jsx
+++ src/components/OurEngagement.jsx
@@ -1,26 +1,24 @@
-
-
 const OurEngagement = () => {
     return (
-        <div className=" xs:px-3 xs:pt-3 sm:px-5  lg:px-20 md:px-[56px] xl:px-[100px] 2xl:px-[120px] 3xl:px-[182px] 3xl:pt-5 w-[100vw] overflow-x-auto">
-            <h2 className="xs:text-2xl sm:w-[357px]  sm:h-[100px]  sm:leading-[42px] text-[#272727]  sm:mb-[28px] sm:text-4xl  3xl:text-[56px]  3xl:w-[508px] 3xl:leading-[67px] font-poppins font-medium">
+        <div className=" xs:px-3 xs:pt-3 sm:px-5  lg:px-20 md:px-14 xl:px-24 2xl:px-28 3xl:px-46 3xl:pt-5 w-screen overflow-x-auto">
+            <h2 className="xs:text-2xl sm:w-94  sm:h-25  sm:leading-11 text-blackish  sm:mb-7 sm:text-4xl  3xl:text-5.5xl  3xl:w-97 3xl:leading-13 font-poppins font-medium">
                 Our Engagement Models
             </h2>
-            <div className=" xs:pt-5 3xl:pt-8 flex flex-col gap-[16.8px] md:flex-row sm:justify-center">
-                <div className=" sm:m-auto w-full xs:h-[300px] xs:pb-5 sm:h-[427px] sm:w-[356px] md:w-[410px] sm:drop-shadow-[16px] lg:w-[570px]  lg:h-[350px] xl:w-[650px] xl:h-[375px] 2xl:w-[710px] 2xl:h-[410px] 3xl:w-[770px] 3xl:h-[578px] bg-[#F7F7F7]  border rounded-lg flex flex-col xs:justify-end 3xl:items-start 3xl:pl-[72px]  items-center">
-                    <h3 className=" xs:text-[18px] xs:w-[180px] xs:mb-3 sm:w-[232px] lg:w-[300px]  sm:h-[82px]  sm:leading-[33.6px]  font-poppins font-medium sm:text-[28px] lg:text-3xl  3xl:text-4xl opacity-[1] text-left">
+            <div className=" xs:pt-5 3xl:pt-8 flex flex-col gap-4 md:flex-row sm:justify-center">
+                <div className=" sm:m-auto w-full xs:h-73 xs:pb-5 sm:h-96 sm:w-81 md:w-97 sm:shadow-md lg:w-98  lg:h-82 xl:w-108 xl:h-83 2xl:w-116 2xl:h-95 3xl:w-120 3xl:h-102 bg-bgPrimary  border rounded-lg flex flex-col xs:justify-end 3xl:items-start 3xl:pl-18  items-center">
+                    <h3 className=" xs:text-lg xs:w-45 xs:mb-3 sm:w-57 lg:w-73  sm:h-21  sm:leading-12  font-poppins font-medium sm:text-2.5xl lg:text-3xl  3xl:text-4xl opacity-100 text-left">
                         Monthly <br /> Retainer
                     </h3>
-                    <p className=" xs:text-[12px] xs:w-[180px] sm:w-[232px] lg:w-[300px] 3xl:text-2xl 3xl:w-[393px] font-roboto font-regular sm:text-lg opacity-[0.8] sm:pb-[39px]">
+                    <p className=" xs:text-xs xs:w-45 sm:w-57 lg:w-73 3xl:text-2xl 3xl:w-84 font-roboto font-regular sm:text-lg opacity-80 sm:pb-9.5">
                         Yume Labs is a global UI UX Design and Industrial Design agency that helps your business scale
                         through fail-proof design innovation systems.
                     </p>
                 </div>
-                <div className=" sm:m-auto w-full xs:h-[300px] xs:pb-5 sm:h-[427px] sm:w-[356px] md:w-[410px] sm:drop-shadow-[16px] lg:w-[570px]  lg:h-[350px] xl:w-[650px] xl:h-[375px] 2xl:w-[710px] 2xl:h-[410px] 3xl:w-[770px] 3xl:h-[578px] bg-[#F7F7F7]  border rounded-lg flex flex-col xs:justify-end 3xl:items-start 3xl:pl-[72px]  items-center">
-                    <h3 className=" xs:text-[18px] xs:w-[180px] xs:mb-3 sm:w-[232px] lg:w-[300px]  sm:h-[82px]  sm:leading-[33.6px]  font-poppins font-medium sm:text-[28px] lg:text-3xl  3xl:text-4xl opacity-[1] text-left">
+                <div className=" sm:m-auto w-full xs:h-73 xs:pb-5 sm:h-96 sm:w-81 md:w-97 sm:shadow-md lg:w-98  lg:h-82 xl:w-108 xl:h-83 2xl:w-116 2xl:h-95 3xl:w-120 3xl:h-102 bg-bgPrimary  border rounded-lg flex flex-col xs:justify-end 3xl:items-start 3xl:pl-18  items-center">
+                    <h3 className=" xs:text-lg xs:w-45 xs:mb-3 sm:w-57 lg:w-73  sm:h-21  sm:leading-12  font-poppins font-medium sm:text-2.5xl lg:text-3xl  3xl:text-4xl opacity-100 text-left">
                         Fixed <br /> Price
                     </h3>
-                    <p className=" xs:text-[12px] xs:w-[180px] sm:w-[232px] lg:w-[300px] 3xl:text-2xl 3xl:w-[393px] font-roboto font-regular sm:text-lg opacity-[0.8] sm:pb-[39px]">
+                    <p className=" xs:text-xs xs:w-45 sm:w-57 lg:w-73 3xl:text-2xl 3xl:w-84 font-roboto font-regular sm:text-lg opacity-80 sm:pb-9.5">
                         Yume Labs is a global UI UX Design and Industrial Design agency that helps your business scale
                         through fail-proof design innovation systems.
                     </p>
