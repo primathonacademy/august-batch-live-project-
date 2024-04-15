c3149153737ffea0e9344f19439dfaadfb699133
Meena Dhakad <dhakadm970@gmail.com>
2024-04-15 11:02:19 +0530

    All done

--- src/components/Home/NewsCard.jsx
+++ src/components/Home/NewsCard.jsx
@@ -1,17 +1,18 @@
 import newsCard from '../../assets/images/newscard.png';
+
 function NewsCard() {
     return (
-        <div className="flex overflow-x-auto lg:overflow-hidden lg:scrollbar-hide gap-4 w-full px-5 md:px-0">
+        <div className="flex overflow-x-auto lg:overflow-hidden lg:scrollbar-hide gap-4 w-full md:px-0">
             <div className="w-full flex ">
-                <div className="flex flex-no-wrap w-auto ">
+                <div className="flex flex-no-wrap w-auto sm:px-5 md:px-0 xl:px-5">
                     <div className="w-full mr-4">
                         <div className="w-56 lg:w-auto md:w-full">
                             <img src={newsCard} alt="News" className="bg-light-blue w-full rounded-md bg-slate-200" />
                             <div className="mt-2">
-                                <h1 className=" font-semibold text-lg pt-4 3xl:text-2xl  4xl:text-4xl ">
+                                <h1 className="font-semibold text-lg pt-4 xl:text-2xl">
                                     /01/ News Technology / 03 March 2022
                                 </h1>
-                                <p className="font-roboto text-gray-600 pt-4 text-lg lg:w-8/12 3xl:text-2xl  4xl:text-4xl 4xl:w-full">
+                                <p className="font-roboto text-gray-600 pt-4 text-lg lg:w-8/12 xl:text-2xl">
                                     Great Place to Work 2022 Survey: Vincit is the best place to work in Finland, again!
                                 </p>
                             </div>
@@ -19,12 +20,12 @@ function NewsCard() {
                     </div>
                     <div className="w-full mr-4">
                         <div className="w-56 lg:w-auto md:w-full">
-                            <img src={newsCard} alt="News" className="w-full  rounded-lg bg-slate-200" />
+                            <img src={newsCard} alt="News" className="w-full rounded-lg bg-slate-200" />
                             <div className="mt-2">
-                                <h1 className=" font-semibold text-lg pt-4 3xl:text-2xl  4xl:text-4xl ">
+                                <h1 className="font-semibold text-lg pt-4 xl:text-2xl">
                                     /01/ News Technology / 03 March 2022
                                 </h1>
-                                <p className=" font-roboto text-gray-600 pt-4 text-lg lg:w-8/12 3xl:text-2xl  4xl:text-4xl 4xl:w-full">
+                                <p className="font-roboto text-gray-600 pt-4 text-lg lg:w-8/12 xl:text-2xl">
                                     Great Place to Work 2022 Survey: Vincit is the best place to work in Finland, again!
                                 </p>
                             </div>
@@ -32,12 +33,12 @@ function NewsCard() {
                     </div>
                     <div className="w-full mr-4">
                         <div className="w-56 lg:w-auto md:w-full">
-                            <img src={newsCard} alt="News" className=" w-full rounded-lg bg-slate-200" />
+                            <img src={newsCard} alt="News" className="w-full rounded-lg bg-slate-200" />
                             <div className="mt-2 pb-28">
-                                <h1 className="  font-semibold text-lg pt-4 3xl:text-2xl  4xl:text-4xl">
+                                <h1 className="font-semibold text-lg pt-4 xl:text-2xl">
                                     /01/ News Technology / 03 March 2022
                                 </h1>
-                                <p className=" text-gray-600  font-roboto pt-4 text-lg lg:w-8/12 3xl:text-2xl  4xl:text-4xl  4xl:w-full">
+                                <p className="font-roboto text-gray-600 pt-4 text-lg lg:w-8/12 xl:text-2xl">
                                     Great Place to Work 2022 Survey: Vincit is the best place to work in Finland, again!
                                 </p>
                             </div>
