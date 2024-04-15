202060f98122a39178106938ab17e77d6e3f425f
Rajiv <rajivkum2021@gmail.com>
2024-04-15 11:05:06 +0530

    [Feature]: Our service poster added

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,10 @@ import App from './pages/App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
-          <App />
+            <App />
         </BrowserRouter>
     </React.StrictMode>
 );
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,23 +1,21 @@
-import { useEffect, useState } from "react";
-import OurServicePosterSection from "../components/OurServicePosterSection";
+import { useEffect, useState } from 'react';
+import OurServicePosterSection from '../components/OurServicePosterSection';
 
 const App = () => {
     const [screenSize, setScreenSize] = useState(window.innerWidth);
-    useEffect(()=>{
-        const handleResize = () =>{
+    useEffect(() => {
+        const handleResize = () => {
             setScreenSize(window.innerWidth);
         };
         window.addEventListener('resize', handleResize);
         return () => window.removeEventListener('resize', handleResize);
-    },[]);
+    }, []);
 
     return (
         <>
-        
-        <OurServicePosterSection screenSize={screenSize}/>
-        
+            <OurServicePosterSection screenSize={screenSize} />
         </>
-    )
+    );
 };
 
 export default App;
