2f6c83006514b3a177241bae7328a48476ec1a47
DISCOVERANNY <akyblp@gmail.com>
2024-04-16 11:22:27 +0530

    [Feature]: Add our work section

---
+++ src/components/WorkSection.jsx
@@ -0,0 +1,113 @@
+
+import image1 from '../assets/image/tablet.png';
+import image2 from '../assets/image/black-display.png';
+import image3 from '../assets/image/45 inclined.png';
+import image4 from '../assets/image/blue-display.png';
+
+const WorkSection = () => {
+    return (
+        <>
+            <div className="px-5">
+                <div className="mt-4">
+                    <h1 className="bg-indigo-400 italic font-semibold font-serif text-center w-[100px]  mb-2.5 3xl:w-[180px] 3xl:text-4xl ">
+                        Our Work
+                    </h1>
+                    <p className=" 3xl:mb-[90px] text-2xl text-[#0B1226] xs:text-4xl 3xl:text-[56px] 3xl:leading-[64px] 3xl:font-poppins xs:h-[231px] xs:w-[356px] 3xl:w-[1235px] 3xl:[245px] 3xl:font-semibold  font-Poppins text-left lg:w-full lg:h-[200px] ">
+                        Discover brand innovation stories that will inspire you to reimagine the Next for yours.
+                    </p>
+                </div>
+                <div className="flex flex-wrap gap-4 3xl:gap-28">
+                    <div className="mt-2  ">
+                        <img
+                            src={image1}
+                            alt="mobile display"
+                            className="w-full xs:h-[422px] xs:w-[356px] lg:w-[300px] 3xl:h-[852px] 3xl:w-[722px] 3xl:mb-[71px}"
+                        />
+
+                        <div className="flex gap-2.5 mt-8 font-poppins font-semibold text-xs text-[#253F9C] 3xl:mt-8 3xl:[23px] 3xl:text-3xl 3xl:mb-1 3xl:p-4 ">
+                            <p># UX/UI</p>
+                            <p>#DEVELOPMENT</p>
+                        </div>
+
+                        <h1 className="mt-2 text-[#0B1226] text-[28px] leading-[43px] w-[300px] h-7 font-Poppins font-medium 3xl:font-poppins 3xl:font-medium 3xl:mb-6 3xl:mt-0">
+                            Simplicontract
+                        </h1>
+
+                        <p className="mt-6 h-28 font-roboto font-regular text-[18px] leading-[24px] text-[#7B7B7B] xs:w-[356px] 3xl:w-[644px] 3xl:[133px] 3xl:font-roboto 3xl:text-[24px] 3xl:text-[#7B7B7B] 3xl:mt-1">
+                            Yumelabs is a global UI UX Design and Industrial Design agency that helps your business
+                            scale through fail-proof design innovation systems.
+                        </p>
+                    </div>
+
+                    <div className="mt-40  ">
+                        <img
+                            src={image2}
+                            alt="mobile display"
+                            className="w-full xs:h-[422px] xs:w-[356px] lg:w-[300px] 3xl:w-[720px] 3xl:h-[855px] "
+                        />
+
+                        <div className="flex gap-2.5 mt-8 font-poppins font-semibold text-xs text-[#253F9C] 3xl:h-[280px] 3xl:[23px] 3xl:text-3xl 3xl:mb-6">
+                            <p># UX/UI</p>
+                            <p>#DEVELOPMENT</p>
+                        </div>
+
+                        <h1 className="mt-2 text-[#0B1226] text-[28px] leading-[43px] w-[300px] h-7 font-Poppins font-medium 3xl:font-poppins 3xl:font-medium 3xl:mb-10">
+                            Simplicontract
+                        </h1>
+
+                        <p className="mt-6 h-28 font-roboto font-regular text-[18px] leading-[24px] text-[#7B7B7B] xs:w-[356px] 3xl:text-{#7B7B7B} 3xl:w-[644px] 3xl:[133px] 3xl:font-roboto 3xl:text-[24px]">
+                            Yumelabs is a global UI UX Design and Industrial Design agency that helps your business
+                            scale through fail-proof design innovation systems.
+                        </p>
+                    </div>
+
+                    <div className="mt-2 ">
+                        <img
+                            src={image3}
+                            alt="mobile display"
+                            className="w-full xs:h-[422px] xs:w-[356px] 3xl:w-[721px] 3xl:h-[853px] "
+                        />
+
+                        <div className="flex gap-2.5 mt-8 font-poppins font-semibold text-xs text-[#253F9C] 3xl:h-[280px] 3xl:[23px] 3xl:text-3xl 3xl:mb-6">
+                            <p># UX/UI</p>
+                            <p>#DEVELOPMENT</p>
+                        </div>
+
+                        <h1 className="mt-2 text-[#0B1226] text-[28px] leading-[43px] w-[300px] h-7 font-Poppins font-medium 3xl:font-poppins 3xl:font-medium 3xl:mb-10">
+                            Simplicontract
+                        </h1>
+
+                        <p className="mt-6 h-28 font-roboto font-regular text-[18px] leading-[24px] text-[#7B7B7B] xs:w-[356px] 3xl:text-{#7B7B7B} 3xl:w-[644px] 3xl:[133px] 3xl:font-roboto 3xl:text-[24px] 3xl:mt-11">
+                            Yumelabs is a global UI UX Design and Industrial Design agency that helps your business
+                            scale through fail-proof design innovation systems.
+                        </p>
+                    </div>
+
+                    <div className="mt-40 ">
+                        <img
+                            src={image4}
+                            alt="mobile display"
+                            className="w-full xs:h-[422px] xs:w-[356px] 3xl:w-[719px] 3xl:h-[849px]"
+                        />
+
+                        <div className="flex gap-2.5 mt-8 font-poppins font-semibold text-xs text-[#253F9C] 3xl:h-[280px] 3xl:[23px] 3xl:text-3xl 3xl:mb-6">
+                            <p># UX/UI</p>
+                            <p>#DEVELOPMENT</p>
+                        </div>
+
+                        <h1 className="mt-2 text-[#0B1226] text-[28px] leading-[43px] w-[300px] h-7 font-Poppins font-medium 3xl:font-poppins 3xl:font-medium 3xl:mb-10">
+                            Simplicontract
+                        </h1>
+
+                        <p className="mt-6 h-28 font-roboto font-regular text-[18px] leading-[24px] text-[#7B7B7B] xs:w-[356px] 3xl:text-{#7B7B7B} 3xl:w-[644px] 3xl:[133px] 3xl:font-roboto 3xl:text-[24px]">
+                            Yumelabs is a global UI UX Design and Industrial Design agency that helps your business
+                            scale through fail-proof design innovation systems.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default WorkSection;
