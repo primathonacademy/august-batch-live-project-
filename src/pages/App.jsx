9397c7346c9a292cbf635bdd1bdc28bc43a7f01c
Meena Dhakad <dhakadm970@gmail.com>
2024-04-17 23:37:21 +0530

    [Feature]:Fix prettierrc file error

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter } from "react-router-dom";
-import Homepage from "./homepage";
+import { BrowserRouter } from 'react-router-dom';
+import Homepage from './homepage';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Homepage />
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Homepage />
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,15 +1,15 @@
-import ArtSection from "./ArtSection";
-import NewsCards from "./NewsCards";
-import ServiceBtn from "./ServiceBtn";
+import ArtSection from './ArtSection';
+import NewsCards from './NewsCards';
+import ServiceBtn from './ServiceBtn';
 
 function Homepage() {
-  return (
-    <div>
-      <ArtSection />
-      <NewsCards />
-      <ServiceBtn />
-    </div>
-  );
+    return (
+        <div>
+            <ArtSection />
+            <NewsCards />
+            <ServiceBtn />
+        </div>
+    );
 }
 
 export default Homepage;
