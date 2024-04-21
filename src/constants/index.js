7e31bd37647846b29719b0df7b7173a340c0af08
sakshimandloi <sakshimandloi4722@gmail.com>
2024-04-21 14:48:07 +0530

    [Feature]: Add new update ourengagement model component

---
+++ src/constants/index.js
@@ -0,0 +1,24 @@
+export const ROUTES = {
+    HOME: '/',
+    ABOUT: '/about',
+    CONTACT: '/contact',
+    CASE_STUDY: '/case-study',
+    INSIGHT: '/insights',
+    INSIGHT_DETAIL: '/insight-detail',
+    WORK: '/work',
+    SERVICE: '/service',
+    PRIVACY_POLICY: '/privacy-policy',
+    TERMS_OF_USE: '/terms-of-use',
+    COPYRIGHT: '/copyright-yume-labs'
+};
+
+ export const OUR_ENGAGEMENT_DATA =[
+    {
+        heading:"Monthly Retainer",
+        discription:"Yume Labs is a global UI UX Design and Industrial Design agency that helps your business scale through fail-proof design innovation systems.",
+    },
+    {
+        heading:"Fixed Price",
+        discription:"Yume Labs is a global UI UX Design and Industrial Design agency that helps your business scale through fail-proof design innovation systems.",    
+    }
+]
--- src/pages/service/OurEngagement.jsx
+++ src/pages/service/OurEngagement.jsx
@@ -1,32 +1,30 @@
+import { OUR_ENGAGEMENT_DATA } from '../../constants';
+
 const OurEngagement = () => {
     return (
-        <div className=" w-scree overflow-x-auto xs:px-3  xs:pt-3 sm:px-5 md:px-14 lg:px-20 xl:px-24 2xl:px-28 3xl:px-45.5 3xl:pt-5">
-            <h2 className="font-poppins font-medium  text-zinc-850  xs:text-4xl sm:mb-7  sm:h-25 w-79.5  leading-16.75 3xl:w-127 3xl:text-5.7xl 3xl:leading-16.75">
+        <div className=" w-screen px-5  xs:pt-3 md:px-14 lg:px-20 xl:px-24 2xl:px-28 3xl:px-45.5 3xl:pt-5">
+            <h2 className="font-poppins font-medium  text-zinc-850  text-4xl mb-6">
                 Our Engagement Models
             </h2>
-            <div className=" flex flex-col gap-4 xs:pt-5 xs:justify-center items-center md:flex-row 3xl:pt-8">
-                <div className=" flex xs:w-88.75 flex-col items-center rounded-lg border bg-gray-75 xs:h-au xs:justify-end  xs:pb-5  sm:h-106.7 sm:w-89 sm:shadow-md md:w-127 lg:h-87.5 lg:w-142.4  xl:h-93.7 xl:w-162.5 2xl:h-102.5 2xl:w-177.5 3xl:h-144.5 3xl:w-192.5 3xl:items-start  3xl:pl-18">
-                    <div className=" xs:mr-18 xs:mb-9.7 xs:mt-41 md:ml-4 md:mb-4 xl:pr-16">
-                        <h3 className=" text-left font-poppins font-medium xs:mb-3  xs:w-45  xs:text-lg  sm:h-20.5 sm:w-57 sm:text-2xl sm:leading-8 md:mb-1  lg:w-75 lg:text-3xl 3xl:text-4xl">
-                            Monthly <br /> Retainer
-                        </h3>
-                        <p className=" font-regular font-roboto opacity-80 xs:w-45 xs:text-xs sm:w-58  sm:text-base lg:w-75 3xl:w-98.2 3xl:text-2xl">
-                            3 Yume Labs is a global UI UX Design and Industrial Design agency that helps your business
-                            scale through fail-proof design innovation systems.
-                        </p>
-                    </div>
-                </div>
-                <div className=" flex xs:w-88.75 flex-col items-center rounded-lg border bg-gray-75 xs:h-au xs:justify-end  xs:pb-5  sm:h-106.7 sm:w-89 sm:shadow-md md:w-127 lg:h-87.5 lg:w-142.4  xl:h-93.7 xl:w-162.5 2xl:h-102.5 2xl:w-177.5 3xl:h-144.5 3xl:w-192.5 3xl:items-start  3xl:pl-18">
-                    <div className=" xs:mr-18 xs:mb-9.7 xs:mt-41 md:ml-4 md:mb-4 xl:pr-16">
-                        <h3 className=" text-left font-poppins font-medium xs:mb-3  xs:w-45  xs:text-lg  sm:h-20.5 sm:w-57 sm:text-2xl sm:leading-8 md:mb-1  lg:w-75 lg:text-3xl 3xl:text-4xl">
-                            Fixed <br /> Price
-                        </h3>
-                        <p className=" font-regular font-roboto opacity-80 xs:w-45 xs:text-xs sm:w-58  sm:text-base lg:w-75 3xl:w-98.2 3xl:text-2xl">
-                            Yume Labs is a global UI UX Design and Industrial Design agency that helps your business
-                            scale through fail-proof design innovation systems.
-                        </p>
-                    </div>
-                </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-5">
+                {
+                OUR_ENGAGEMENT_DATA.map((item, i) => {
+                    return (
+                        <div
+                            key={i}
+                            className=" flex flex-col items-center rounded-lg border p-16 bg-gray-75 w-full 3xl:py-20">
+                            <div className="pt-14 pr-18 2xl:pr-24">
+                                <h3 className=" text-left font-poppins font-medium text-2.5xl mb-4 w-10 2xl:text-4xl">
+                                    {item.heading}
+                                </h3>
+                                <p className=" font-regular font-roboto opacity-80 lg:text-lg xl:w-110 2xl:text-2xl">
+                                    {item.discription}
+                                </p>
+                            </div>
+                        </div>
+                    );
+                })
+                }
             </div>
         </div>
     );
