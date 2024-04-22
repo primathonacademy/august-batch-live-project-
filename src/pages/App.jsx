c3ec26f09b7ddded26b8b0ba8b8ba6b603c788ad
Jitendra Sharma <sharmajn1211@gmail.com>
2024-04-22 07:46:03 +0530

    [Feature]: Add update customer love component

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -8,7 +8,7 @@ import InsightDetail from './insight-detail';
 import Insight from './insights';
 import Service from './service';
 import Work from './work';
-import { ROUTES } from '../constants/';
+import { ROUTES } from '../constants/app-constant';
 
 const App = () => {
     return (
