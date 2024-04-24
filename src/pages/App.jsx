88c407566e6435a49f80ecf086bd9e8b28d4433b
Meena Dhakad <dhakadm970@gmail.com>
2024-04-24 10:42:44 +0530

    [Feature] : Add routing

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -8,19 +8,22 @@ import InsightDetail from './insight-detail';
 import Work from './work';
 import Service from './service';
 import { ROUTES } from '../constants';
+import Layout from '../Layout';
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path={ROUTES.HOME} element={<Homepage />} />
-                <Route path={ROUTES.ABOUT} element={<About />} />
-                <Route path={ROUTES.CONTACT} element={<Contact />} />
-                <Route path={ROUTES.CASE_STUDY} element={<CaseStudy />} />
-                <Route path={ROUTES.INSIGHT} element={<Insights />} />
-                <Route path={ROUTES.INSIGHT_DETAIL} element={<InsightDetail />} />
-                <Route path={ROUTES.WORK} element={<Work />} />
-                <Route path={ROUTES.SERVICE} element={<Service />} />
-                <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+                <Route element={<Layout />}>
+                    <Route path={ROUTES.HOME} element={<Homepage />} />
+                    <Route path={ROUTES.ABOUT} element={<About />} />
+                    <Route path={ROUTES.CONTACT} element={<Contact />} />
+                    <Route path={ROUTES.CASE_STUDY} element={<CaseStudy />} />
+                    <Route path={ROUTES.INSIGHT} element={<Insights />} />
+                    <Route path={ROUTES.INSIGHT_DETAIL} element={<InsightDetail />} />
+                    <Route path={ROUTES.WORK} element={<Work />} />
+                    <Route path={ROUTES.SERVICE} element={<Service />} />
+                    <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
