6667d333420867fd8dcf57836d268014ed65507d
unknown <imtriloksingh2000@gmail.com>
2024-04-28 20:02:33 +0530

    [Feature]: Add form section

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,5 +1,20 @@
+import FormSection from './FormSection';
+import { useState, useEffect } from 'react';
+
 function Contact() {
-    return <div></div>;
+    const [screenSize, setScreenSize] = useState(window.innerWidth);
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenSize(window.innerWidth);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+    return (
+        <div>
+            <FormSection screenSize={screenSize} />
+        </div>
+    );
 }
 
 export default Contact;
