495638fac22902f126207b0ea1d402ec74f7c71e
Jitendra Sharma <js.sharma124@gmail.com>
2024-05-07 12:14:58 +0530

    [Feature]: Add founder-data in constant folder

--- src/pages/About/FounderSection.jsx
+++ src/pages/About/FounderSection.jsx
@@ -1,32 +1,6 @@
-import AbhinavkumarFounder from '../../assets/images/abhinav-kumar-founder.png';
-import NabarunBhowmickFounder from '../../assets/images/nabarun-bhowmick-founder.png';
-import PritamSahaFounder from '../../assets/images/pritam-saha-founder.png';
 import LinkedInIcon from '../../assets/images/linkedin-img.svg';
 import GridImg from '../../assets/images/grid-image.svg';
-
-const FOUNDER_CARD = [
-    {
-        profile_image_url: `${AbhinavkumarFounder}`,
-        founder: 'Abhinav Kr.',
-        designation: 'Chief Technology Officer',
-        desc: 'Abhinav leads Experience and Interaction Design for our clients.He is a serial technology entrepreneur(ex - IndieBio and Google Accelerator cohorts) and a published researcher and subject matter expert in Computational Neuroscience and Brain - Machine Interfaces.He is an M.Des(Interaction Design) from IDC - IIT Bombay and B.Tech in Mechanical Engineering from IIIT DM, Jabalpur.',
-        alt_name: 'abhinav kumar founder'
-    },
-    {
-        profile_image_url: `${NabarunBhowmickFounder}`,
-        founder: 'Nabarun Bhowmick',
-        designation: 'Chief Design Officer',
-        desc: ' Nabarun leads Industrial and Marketing Design for our clients. He has worked extensively with Exploration & Innovation teams in large Corporates in the Appliances and Healthcare domains globally and has multiple patents and awards to his name.He is an M.Des(Industrial Design) from IDC - IIT Bombay and B.E.in Electronics and Telecommunications from IIEST, Shibpur.',
-        alt_name: 'nabarun bhowmick founder'
-    },
-    {
-        profile_image_url: `${PritamSahaFounder}`,
-        founder: 'Pritam Saha',
-        designation: 'Chief Executive Officer',
-        desc: 'Pritam leads Marketing and Go-to-Market with a consultative approach to the business aspects of Experience Design. He brings extensive experience in B2B Technology Sales &Marketing, Corporate Development, Strategy, and Consulting in multiple industries globally. He is an MBA from IIM Bangalore and B.E. in Electronics and Telecommunications from IIEST, Shibpur.',
-        alt_name: 'pritam saha founder'
-    }
-];
+import FOUNDER_DATA from '../../constants/founder-data';
 
 const FounderSection = () => {
     return (
@@ -40,7 +14,7 @@ const FounderSection = () => {
                     src={GridImg}
                 />
                 <div className="lg:flex lg:flex-row lg:gap-16 xl:gap-20 items-center justify-center">
-                    {FOUNDER_CARD.map((founder, index) => {
+                    {FOUNDER_DATA.map((founder, index) => {
                         return (
                             <div key={index} className="relative pt-24 w-60 lg:w-80 mx-auto lg:mx-0">
                                 <div className="group relative rounded-tl-lg rounded-tr-lg bg-gray-100 overflow-hidden">
