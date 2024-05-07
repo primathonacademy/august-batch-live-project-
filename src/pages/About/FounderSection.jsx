61a8a821ac89a4eb32c9d7606c29e3260dbb7b32
Jitendra Sharma <js.sharma124@gmail.com>
2024-05-07 10:46:08 +0530

    [Feature]: Update founder images

--- src/pages/About/FounderSection.jsx
+++ src/pages/About/FounderSection.jsx
@@ -1,4 +1,6 @@
 import AbhinavkumarFounder from '../../assets/images/abhinav-kumar-founder.png';
+import NabarunBhowmickFounder from '../../assets/images/nabarun-bhowmick-founder.png';
+import PritamSahaFounder from '../../assets/images/pritam-saha-founder.png';
 import LinkedInIcon from '../../assets/images/linkedin-img.svg';
 import GridImg from '../../assets/images/grid-image.svg';
 
@@ -11,14 +13,14 @@ const FOUNDER_CARD = [
         alt_name: 'abhinav kumar founder'
     },
     {
-        profile_image_url: `${AbhinavkumarFounder}`,
+        profile_image_url: `${NabarunBhowmickFounder}`,
         founder: 'Nabarun Bhowmick',
         designation: 'Chief Design Officer',
         desc: ' Nabarun leads Industrial and Marketing Design for our clients. He has worked extensively with Exploration & Innovation teams in large Corporates in the Appliances and Healthcare domains globally and has multiple patents and awards to his name.He is an M.Des(Industrial Design) from IDC - IIT Bombay and B.E.in Electronics and Telecommunications from IIEST, Shibpur.',
         alt_name: 'nabarun bhowmick founder'
     },
     {
-        profile_image_url: `${AbhinavkumarFounder}`,
+        profile_image_url: `${PritamSahaFounder}`,
         founder: 'Pritam Saha',
         designation: 'Chief Executive Officer',
         desc: 'Pritam leads Marketing and Go-to-Market with a consultative approach to the business aspects of Experience Design. He brings extensive experience in B2B Technology Sales &Marketing, Corporate Development, Strategy, and Consulting in multiple industries globally. He is an MBA from IIM Bangalore and B.E. in Electronics and Telecommunications from IIEST, Shibpur.',
