d24f27122d0763fc3f424c8384f533ff615cc835
aakash <aakash96710@gmail.com>
2024-05-07 12:16:45 +0530

    Remove logoname image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import yumeLabsNameLogo from '../assets/images/yume-labs-name-logo.svg';
-import BrandName from '../assets/images/header-logo-name-image.svg';
 import { NAV_ITEMS } from '../constants';
 import { ROUTES } from '../constants';
 
@@ -32,7 +31,7 @@ const Header = () => {
     };
 
     const handleNavItemClick = () => {
-        setIsMenuOpen(false); // Close the menu when a navigation item is clicked
+        setIsMenuOpen(false); 
     };
 
     const NAVBAR_ITEMS_EXCLUDING_LAST = NAV_ITEMS.slice(0, -1);
@@ -44,22 +43,14 @@ const Header = () => {
                     isMenuOpen ? 'menu-open' : ''
                 } ${isScrolled ? 'backdrop-blur-md bg-opacity-75' : ''}`}>
                 <div className="flex h-16 md:border-0 justify-between items-center">
-                    <div className="flex gap-4 items-center md:gap-2 lg:gap-3 xl:gap-4 2xl:gap-4 3xl:gap-5">
-                        <div>
+                        <Link to="/">
                             <img
                                 src={yumeLabsNameLogo}
                                 alt="yumi lab logo "
-                                className="h-6 w-4 md:h-6 md:w-4 lg:h-7 lg:w-5 xl:h-7 xl:w-5 2xl:h-8 2xl:w-5 3xl:h-8 3xl:w-6 4xl:h-9 4xl:w-7"
+                                className="w-36 md:w-40 lg:h-8 lg:w-60"
                             />
-                        </div>
-                        <span>
-                            <img
-                                src={BrandName}
-                                alt="yumi lab brand name "
-                                className="md:w-32 h-5 w-36 md:h-5 lg:h-5 lg:w-40 xl:h-5 xl:w-40 2xl:h-6 2xl:w-44 3xl:h-6 3xl:w-45.5 4xl:h-7 4xl:w-48"
-                            />
-                        </span>
-                    </div>
+                       </Link>
+                        
                     <div className="md:hidden">
                         <button onClick={toggleMenu}>{isMenuOpen ? <FaTimes /> : <FaBars />}</button>
                     </div>
