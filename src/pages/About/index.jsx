0908f99a5985d07e75a4afa246245aff5f126283
Rahul saini <rs7965498@gmail.com>
2024-05-12 19:02:27 +0530

    [Feature]: fix layout issues

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from 'react';
 import AboutSection from './AboutSection';
 
 const About = () => {
-    return <AboutSection />;
+    const [screenSize, setScreenSize] = useState(window.innerWidth);
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenSize(window.innerWidth);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+    return (
+        <>
+            <AboutSection screenSize={screenSize} />
+        </>
+    );
 };
 
 export default About;
