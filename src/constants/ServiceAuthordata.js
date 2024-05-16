2a9a182b1baa74dcbae7c503655c8a520281dc4c
Meena Dhakad <dhakadm970@gmail.com>
2024-05-16 09:49:42 +0530

    [BugFix]: Resolve padding and margin

--- src/constants/ServiceAuthordata.js
+++ src/constants/ServiceAuthordata.js
@@ -2,11 +2,11 @@ export const AUTHORS = [
     {
         name: 'Abhinav Kr.',
         role: 'Cheif Technology Officer',
-        bio: 'Abhinav leads all things technology at Yume Labs. He brings 10+ years of experience in B2B Technology Sales, Corporate Development, Strategy, and Consulting in Life Sciences and Healthcare industries globally. He holds an MBA from IIM Bangalore and a B.E. in Electronics and Telecommunications from IIEST, Shibpur.'
+        bio: 'Pritam leads all things business at Yume Labs. He brings 10+ years of experience in B2B Technology Sales, Corporate Development, Strategy, and Consulting in Life Sciences and Healthcare industries globally. He is an MBA from IIM Bangalore and B.E. in Electronics and Telecommunications from IIEST, Shibpur.'
     },
     {
         name: 'Anubhav Kr.',
         role: 'Cheif Technology Officer',
-        bio: 'Abhinav leads all things technology at Yume Labs. He brings 10+ years of experience in B2B Technology Sales, Corporate Development, Strategy, and Consulting in Life Sciences and Healthcare industries globally. He holds an MBA from IIM Bangalore and a B.E. in Electronics and Telecommunications from IIEST, Shibpur.'
+        bio: 'Pritam leads all things business at Yume Labs. He brings 10+ years of experience in B2B Technology Sales, Corporate Development, Strategy, and Consulting in Life Sciences and Healthcare industries globally. He is an MBA from IIM Bangalore and B.E. in Electronics and Telecommunications from IIEST, Shibpur.'
     }
 ];
