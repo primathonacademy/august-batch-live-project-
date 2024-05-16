2e5da02f63d90b91749c9ccae42be355745839e5
Kuldeep-primathon <162573156+Kuldeep-primathon@users.noreply.github.com>
2024-05-16 10:04:17 +0530

    Revert "Bugfix/insight detail"

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,33 +1,5 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import About from './about';
-import CaseStudy from './case-study';
-import Contact from './contact';
-import InsightDetail from './insight-detail';
-import Work from './work';
-import Insights from './insights';
-import Service from './service';
-import Layout from '../Layout';
-import HomePage from './homepage';
-import { ROUTES } from '../constants';
-
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route element={<Layout />}>
-                    <Route path={ROUTES.HOME} element={<HomePage />} />
-                    <Route path={ROUTES.CONTACT} element={<Contact />} />
-                    <Route path={ROUTES.ABOUT} element={<About />} />
-                    <Route path={ROUTES.CASE_STUDY} element={<CaseStudy />} />
-                    <Route path={ROUTES.INSIGHT} element={<Insights />} />
-                    <Route path={ROUTES.INSIGHT_DETAIL} element={<InsightDetail />} />
-                    <Route path={ROUTES.WORK} element={<Work />} />
-                    <Route path={ROUTES.SERVICE} element={<Service />} />
-                    <Route path="*" element={<h1>404 - Page Not Found</h1>} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default App;
