434d84a70226ad43439dc9169a4ecea020ad49de
Jitendra Sharma <js.sharma124@gmail.com>
2024-05-22 09:51:09 +0530

    [BugFix]: Add caraousel in customer love section in homepage and in selected project in service page

--- src/pages/service/SelectedProjects.jsx
+++ src/pages/service/SelectedProjects.jsx
@@ -2,11 +2,12 @@ import PropTypes from 'prop-types';
 import { projectData } from '../../constants/project-data';
 import LeftArrowIcon from '../../assets/icons/arrow-left-icon.svg';
 import RightArrowIcon from '../../assets/icons/arrow-right.svg';
+import { useState } from 'react';
 
-const ProjectCard = ({ screenSize }) => {
+const ProjectCard = ({ currentIndex, screenSize }) => {
     return (
         <>
-            {projectData.map((item) => {
+            {projectData.slice(currentIndex, currentIndex + 3).map((item) => {
                 return (
                     <div className="pb-5 ml-2 lg:ml-0 lg:w-80 xl:w-127 3xl:w-175" key={item.id}>
                         {screenSize < 1024 ? (
@@ -34,6 +35,18 @@ const ProjectCard = ({ screenSize }) => {
 };
 
 const SelectedProjects = ({ screenSize }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const nextSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % projectData.length);
+    };
+
+    const prevSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + projectData.length) % projectData.length);
+    };
+    if (currentIndex > projectData.length - 3) {
+        setCurrentIndex(0);
+    }
     return (
         <div>
             <h1 className="text-4xl mb-10 w-40 lg:pl-3 xl:text-5xl xl:leading-12 2xl:text-5.7xl xl:w-60 3xl:leading-15 3xl:pl-2">
@@ -42,7 +55,7 @@ const SelectedProjects = ({ screenSize }) => {
 
             <div className="overflow-x-auto lg:overflow-hidden scrollbar-hide gap-4 md:px-0 w-full lg:flex lg:justify-center 2xl:mx-auto">
                 <div className="flex flex-no-wrap md:w-fit gap-3 lg:gap-5 xl:w-full xl:justify-between 3xl:h-210">
-                    <ProjectCard screenSize={screenSize} />
+                    <ProjectCard currentIndex={currentIndex} screenSize={screenSize} />
                 </div>
             </div>
 
@@ -50,12 +63,22 @@ const SelectedProjects = ({ screenSize }) => {
                 <div className="w-full h-px opacity-70 bg-gray-475"></div>
 
                 <div className="flex items-center">
-                    <img src={LeftArrowIcon} alt="left scrolling arrow img" className="w-16 h-12 3xl:w-16" />
-                    <img
-                        src={RightArrowIcon}
-                        alt="right scrolling arrow img"
-                        className="w-16 h-12 3xl:w-28 3xl:pr-10"
-                    />
+                    <button
+                        disabled={currentIndex === 0 ? true : false}
+                        className={currentIndex === 0 ? `cursor-not-allowed` : ``}
+                        onClick={prevSlide}>
+                        <img src={LeftArrowIcon} alt="left scrolling arrow img" className="w-16 h-12 3xl:w-16" />
+                    </button>
+                    <button
+                        disabled={currentIndex === projectData.length - 3 ? true : false}
+                        className={currentIndex === projectData.length - 3 ? `cursor-not-allowed` : ``}
+                        onClick={nextSlide}>
+                        <img
+                            src={RightArrowIcon}
+                            alt="right scrolling arrow img"
+                            className="w-16 h-12 3xl:w-28 3xl:pr-10"
+                        />
+                    </button>
                 </div>
             </div>
         </div>
@@ -63,10 +86,12 @@ const SelectedProjects = ({ screenSize }) => {
 };
 
 SelectedProjects.propTypes = {
-    screenSize: PropTypes.number.isRequired
+    screenSize: PropTypes.number.isRequired,
+    currentIndex: PropTypes.number.isRequired
 };
 ProjectCard.propTypes = {
-    screenSize: PropTypes.number.isRequired
+    screenSize: PropTypes.number.isRequired,
+    currentIndex: PropTypes.number.isRequired
 };
 
 export default SelectedProjects;
